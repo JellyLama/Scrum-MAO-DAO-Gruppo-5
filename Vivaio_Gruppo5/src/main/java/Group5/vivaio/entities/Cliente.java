313282f9edf636c0f6de94a3e6ea29ad2d5bd4e5
JhonnyPol <jpollinari@gmail.com>
2020-04-23 16:57:03 +0200

    Changed

--- Vivaio_Gruppo5/src/main/java/Group5/vivaio/entities/Cliente.java
+++ Vivaio_Gruppo5/src/main/java/Group5/vivaio/entities/Cliente.java
@@ -37,6 +37,9 @@ public class Cliente implements Serializable
     private String nome;
     
     @Basic
+    private String password;
+    
+    @Basic
     private String tipo;
     
     @Basic
--- Vivaio_Gruppo5/src/main/java/Group5/vivaio/entities/Dipendente.java
+++ Vivaio_Gruppo5/src/main/java/Group5/vivaio/entities/Dipendente.java
@@ -41,6 +41,9 @@ public class Dipendente implements Serializable
     private String nome;
     
     @Basic
+    private String password;
+    
+    @Basic
     private String annoAssunzione;
     
     @Basic
