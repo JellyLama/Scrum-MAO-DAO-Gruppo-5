d4733d6c77079a90614f65749853e05019b3ff8b
Brusetti Karim <karimbrus@gmail.com>
2020-04-23 16:32:59 +0200

    Update data.sql

--- Vivaio_Gruppo5/src/main/resources/data.sql
+++ Vivaio_Gruppo5/src/main/resources/data.sql
@@ -1,7 +1,7 @@
-insert into dettagli_attivita (tipo, nome, necessita_piante) values('lavoro', 'potatura', true);
+insert into dettagli_attivita (tipo, nome, necessita_piante, costo_per_ora) values('lavoro', 'potatura', true, 5.0);
 insert into clienti (cognome, nome, tipo, telefono) values('bachir', 'karim', 'base', '3279706977');
 insert into tipi (nome, prezzo) values('tipoRosmarino', 20.0);
 insert into specie (modo_coltivazione, nome) values('acqua e terra', 'specieRosmarino');
-insert into dipendenti (categoria, cognome, nome, anno_assunzione, qualifica, costo_per_ora, specie_id) values('schiavo', 'rossi', 'giulio', '2001', 'schiavo', 5.0, 1);
+insert into dipendenti (categoria, cognome, nome, anno_assunzione, qualifica, specie_id) values('schiavo', 'rossi', 'giulio', '2001', 'schiavo', 1);
 insert into piante (descrizione, nome, stagione_fioritura, specie_id, tipo_id) values('profuma','rosmarino', 'estate', 1, 1);
 insert into attivita (data_effettuazione, data_prenotazione, evaso, cliente_id, dettagli_attivita_id, dipendente_id) values('2001-07-28', '2001-07-28', true, 1, 1, 1);
