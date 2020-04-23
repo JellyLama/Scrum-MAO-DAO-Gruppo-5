771a1257e460b953a078cc48e358044ea7724101
JhonnyPol <jpollinari@gmail.com>
2020-04-23 18:07:48 +0200

    fdf

--- ClientVivaio/public_html/APP.js
+++ ClientVivaio/public_html/APP.js
@@ -1,21 +1,21 @@
 var APP =
         {
-        getPiante: function ()
-                {
+            getPiante: function ()
+            {
 //gets all post from API
                 $.ajax(
                         {
-                        url: "http://localhost:8080/piante",
-                                method: "GET",
-                                success: function (data, status) {
+                            url: "http://localhost:8080/piante",
+                            method: "GET",
+                            success: function (data, status) {
 
                                 APP.showPiante(data);
-                                }
+                            }
                         }
                 );
-                        },
-                showPiante: function (piante)
-                {
+            },
+            showPiante: function (piante)
+            {
                 var tabellaPiante = '<tr>'
                         + '<th>nome</th>'
                         + '<th>descrizione</th>'
@@ -25,93 +25,92 @@ var APP =
                         + '<th>tipo</th>'
                         + '<th>prezzo</th>'
                         + '</tr>';
-                        for (i = 0; i < piante.length; i++) {
-                var id = piante[i].id;
-                        var nome = piante[i].nome;
-                        var descriione = piante[i].descrizione;
-                        var stagioneFioritura = piante[i].stagioneFioritura;
-                        var specie = piante[i].specie.nome;
-                        var modoDiColtivazione = piante[i].specie.modoColtivazione;
-                        var tipo = piante[i].tipo.nome;
-                        var prezzo = piante[i].tipo.prezzo;
-                        tabellaPiante += '<tr>'
-                        + '<td>' + nome + '</td>'
-                        + '<td>' + descriione + '</td>'
-                        + '<td>' + stagioneFioritura + '</td>'
-                        + '<td>' + specie + '</td>'
-                        + '<td>' + modoDiColtivazione + '</td>'
-                        + '<td>' + tipo + '</td>'
-                        + '<td>' + prezzo + '</td>'
-                        + '</tr>';
+                for (i = 0; i < piante.length; i++) {
+                    var id = piante[i].id;
+                    var nome = piante[i].nome;
+                    var descriione = piante[i].descrizione;
+                    var stagioneFioritura = piante[i].stagioneFioritura;
+                    var specie = piante[i].specie.nome;
+                    var modoDiColtivazione = piante[i].specie.modoColtivazione;
+                    var tipo = piante[i].tipo.nome;
+                    var prezzo = piante[i].tipo.prezzo;
+                    tabellaPiante += '<tr>'
+                            + '<td>' + nome + '</td>'
+                            + '<td>' + descriione + '</td>'
+                            + '<td>' + stagioneFioritura + '</td>'
+                            + '<td>' + specie + '</td>'
+                            + '<td>' + modoDiColtivazione + '</td>'
+                            + '<td>' + tipo + '</td>'
+                            + '<td>' + prezzo + '</td>'
+                            + '</tr>';
                 }
                 document.getElementById("piante").innerHTML = tabellaPiante;
-                },
-                getDettagliAttivita: function ()
-                {
+            },
+            getDettagliAttivita: function ()
+            {
                 //gets all post from API
                 $.ajax(
-                {
-                url: "http://localhost:8080/dettagliattivita",
-                        method: "GET",
-                        success: function (data, status) {
+                        {
+                            url: "http://localhost:8080/dettagliattivita",
+                            method: "GET",
+                            success: function (data, status) {
 
-                        APP.showDettagliAttivita(data);
+                                APP.showDettagliAttivita(data);
+                            }
                         }
-                }
                 );
-                },
-                showDettagliAttivita: function (dettagliAttivita)
-                {
+            },
+            showDettagliAttivita: function (dettagliAttivita)
+            {
                 var tabellaDettagliAttivita = '<tr>'
                         + '<th>tipo</th>'
                         + '<th>nome</th>'
                         + '<th>Necessita Piante</th>'
                         + '<th>Costo Per Ora</th>'
                         + '</tr>';
-                        for (i = 0; i < dettagliAttivita.length; i++) {
-                var id = dettagliAttivita[i].id;
-                        var nome = dettagliAttivita[i].tipo;
-                        var descriione = dettagliAttivita[i].nome;
-                        var stagioneFioritura = dettagliAttivita[i].necessitaPiante;
-                        var costoPerOra = dettagliAttivita[i].costoPerOra;
-                        tabellaDettagliAttivita += '<tr>'
-                        + '<td>' + nome + '</td>'
-                        + '<td>' + descriione + '</td>'
-                        + '<td>' + stagioneFioritura + '</td>'
-                        + '<td>' + costoPerOra + '</td>'
-                        + '</tr>';
+                for (i = 0; i < dettagliAttivita.length; i++) {
+                    var id = dettagliAttivita[i].id;
+                    var nome = dettagliAttivita[i].tipo;
+                    var descriione = dettagliAttivita[i].nome;
+                    var stagioneFioritura = dettagliAttivita[i].necessitaPiante;
+                    var costoPerOra = dettagliAttivita[i].costoPerOra;
+                    tabellaDettagliAttivita += '<tr>'
+                            + '<td>' + nome + '</td>'
+                            + '<td>' + descriione + '</td>'
+                            + '<td>' + stagioneFioritura + '</td>'
+                            + '<td>' + costoPerOra + '</td>'
+                            + '</tr>';
                 }
                 document.getElementById("dettagliAttivita").innerHTML = tabellaDettagliAttivita;
-                },
+            },
 
+            insertCliente: function ()
+            {
 
-                insertCliente: function ()
-                {
-
-                var cognome = document.getElementById("cognome").innerHTML;
-                        var nome = document.getElementById("nome").innerHTML;
-                        var password = document.getElementById("password").innerHTML;
-                        var telefono = document.getElementById("telefono").innerHTML;
-                        $.ajax(
+                var cognome = $("#cognome").val();;
+                var nome = $("#nome").val();;
+                var password = $("#password").val();;
+                var telefono = $("#telefono").val();;
+                $.ajax(
                         {
-                        url: "http://localhost:8080/clienti",
-                                method: "POST",
-                                contentType: "application/json",
-                                data: JSON.stringify(
-                                {
-                                cognome: cognome,
+                            url: "http://localhost:8080/clienti",
+                            method: "POST",
+                            contentType: "application/json",
+                            data: JSON.stringify(
+                                    {
+                                        cognome: cognome,
                                         nome: nome,
                                         password: password,
                                         telefono: telefono
 
-                                }
-                                ),
-                                success: function (data, status) {
+                                    }
+                            ),
+                            success: function (data, status) {
 
-                                APP.insertCliente(data);
-                                }
+                            
                             }
-                    );
-                }
+                        }
+                );
+            }
 
-}
+        }
